f074dc35a636d47258f8fe5a134a6b4109fb0074
Peter Winton <pwinton@indigobio.com>
2014-11-21 21:11:01 -0500

    Tree visitor working

--- lib/abstractivator/tree_visitor.rb
+++ lib/abstractivator/tree_visitor.rb
@@ -4,42 +4,65 @@ require 'abstractivator/collections'
 require 'abstractivator/cons'
 
 module Abstractivator
-  class TreeVisitor
-    include Abstractivator::Cons
+  module TreeVisitor
 
-    def self.transform(hash, &block)
-      self.new(block).transform(hash, Cons.empty_list)
+    def transform_tree(hash, &block)
+      Closure.new(block).transform(hash, Cons.empty_list)
     end
 
-    def transform(x, path)
-      case x
-        when Hash
-          Hash[x.map{|kv| [kv.first, transform(kv.last, cons(kv.first.to_s, path))]}]
-        when Array
-          x.each_with_index.map{|v, i| transform(v, cons(i.to_s, path))}
-        else
-          @block.call(Path.new(list_to_enum(path).to_a.reverse), x)
+    class Closure
+      include Abstractivator::Cons
+
+      def transform(x, path)
+        case x
+          when Hash
+            Hash[x.map{|kv| [kv.first, transform(kv.last, cons(kv.first.to_s, path))]}]
+          when Array
+            x.each_with_index.map{|v, i| transform(v, cons(i.to_s, path))}
+          else
+            @block.call(Path.new(list_to_enum(path).to_a.reverse), x)
+        end
+      end
+
+      def initialize(block)
+        @block = block || ->(_, value){value}
       end
     end
 
-    private
-
-    def initialize(block)
-      @block = block || ->(_, value){value}
-    end
-
-    class Path
+    class Path < OpenStruct
       def initialize(names)
+        super(nil)
         @names = names
       end
 
       def to_s
         @names.join('/')
       end
+
+      def ===(other)
+        onames = other.split('/')
+        return false if @names.size != onames.size
+        @names.zip(onames).map {|a, b|
+          bstr = b.to_s
+          if bstr[0] == ':'
+            self[bstr[1..-1].to_sym] = a
+            true
+          else
+            a == b
+          end
+        }.all?
+      end
     end
   end
 end
 
-def transform_tree(*args, &block)
-  Abstractivator::TreeVisitor.transform(*args, &block)
+class String
+  def ===(other)
+    case other
+      when Abstractivator::TreeVisitor::Path
+        other === self
+      else
+        super(other)
+    end
+  end
 end
--- spec/lib/abstractivator/tree_visitor_spec.rb
+++ spec/lib/abstractivator/tree_visitor_spec.rb
@@ -1,24 +1,32 @@
 require 'rspec'
 require 'abstractivator/tree_visitor'
 
+Path = Abstractivator::TreeVisitor::Path
+
 describe Abstractivator::TreeVisitor do
+
+  include Abstractivator::TreeVisitor
 
   let!(:hash) do
     {
       a: {
         b1: {
+          _id: 1,
           c: 'a.b1.c'
         },
         b2: {
+          _id: 2,
           c: 'a.b2.c'
         }
       },
       d: [
            {
+             _id: 3,
              e: 'd.0.e',
              f: 'd.0.f'
            },
            {
+             _id: 3,
              e: 'd.1.e',
              f: 'd.1.f'
            }
@@ -26,12 +34,40 @@ describe Abstractivator::TreeVisitor do
     }
   end
   describe '#visit' do
-    context 'when no block is specified' do
+    context 'when no block is provided' do
       it 'returns a deep copy' do
         result = transform_tree(hash)
         expect(result).to eql hash
         expect(result.equal?(hash)).to be_falsey
       end
+    end
+    context 'when a block is provided' do
+      it 'replaces the value with the return value of the block' do
+        result = transform_tree(hash) do |path, value|
+          case path
+            when 'a/:x/c'
+              "#{path.x}!!"
+            else
+              value
+          end
+        end
+        expect(result[:a][:b1][:c]).to eql 'b1!!'
+        expect(result[:a][:b2][:c]).to eql 'b2!!'
+      end
+    end
+  end
+  describe '::Path' do
+    it 'matches with ===' do
+      path = Path.new(%w(a b c))
+      expect(path === 'a/b/c').to be_truthy
+      expect(path === 'a/b/d').to be_falsey
+      expect(path === 'a/b').to be_falsey
+    end
+    it 'captures match groups' do
+      path = Path.new(%w(a b c d))
+      expect(path === 'a/:x/c/:y').to be_truthy
+      expect(path.x).to eql 'b'
+      expect(path.y).to eql 'd'
     end
   end
 end
