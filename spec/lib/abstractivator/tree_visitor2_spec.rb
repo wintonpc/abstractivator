50ce1c2441675b3ac8efb45c9d878909540ae288
Peter Winton <pwinton@indigobio.com>
2014-11-24 17:43:45 -0500

    Added transform_tree2 tests

--- spec/lib/abstractivator/tree_visitor2_spec.rb
+++ spec/lib/abstractivator/tree_visitor2_spec.rb
@@ -8,33 +8,6 @@ describe Abstractivator::TreeVisitor do
 
   include Abstractivator::TreeVisitor
 
-  let!(:hash) do
-    {
-      a: {
-        b1: {
-          _id: :asdf,
-          c: 'a.b1.c'
-        },
-        b2: {
-          _id: :qwerty,
-          c: 'a.b2.c'
-        }
-      },
-      d: [
-           {
-             _id: :smbc,
-             e: 'd.0.e',
-             f: 'd.0.f'
-           },
-           {
-             _id: :roygbiv,
-             e: 'd.1.e',
-             f: 'd.1.f'
-           }
-         ]
-    }
-  end
-
   describe '#visit' do
 
     context 'when no block is provided' do
@@ -43,73 +16,104 @@ describe Abstractivator::TreeVisitor do
       end
     end
 
-    it 'replaces hash members'
-    it 'replaces array members'
-    it 'replaces hashes'
-    it 'replaces arrays'
-    it 'replaces primitive values'
+    it 'handles both string and symbol keys' do
+      h = {:a => 1, 'b' => 2}
+      result = transform_tree2(h) do |t|
+        t.when('a') {|v| v + 10}
+        t.when('b') {|v| v + 10}
+      end
+      expect(result).to eql({:a => 11, 'b' => 12})
+    end
 
-    context 'when a block is provided' do
-      it 'replaces the value with the return value of the block' do
-        result = transform_tree2(hash) do |t|
-          t.when('a/b1/c') {|v| v.reverse }
-          t.when('a/b2/c') {|v| v.reverse }
+    it 'replaces primitive-type hash fields' do
+      h = {'a' => 1}
+      result = transform_tree2(h) do |t|
+        t.when('a') { 2 }
+      end
+      expect(result).to eql({'a' => 2})
+    end
+
+    it 'replaces hash-type hash fields' do
+      h = {'a' => {'b' => 1}}
+      result = transform_tree2(h) do |t|
+        t.when('a') { {'z' => 99} }
+      end
+      expect(result).to eql({'a' => {'z' => 99}})
+    end
+
+    it 'replaces array-type hash fields' do
+      h = {'a' => [1,2,3]}
+      result = transform_tree2(h) do |t|
+        t.when('a') {|v| v.reverse}
+      end
+      expect(result).to eql({'a' => [3,2,1]})
+    end
+
+    it 'replaces primitive-type hash members' do
+      h = {'a' => {'b' => 'foo', 'c' => 'bar'}}
+      result = transform_tree2(h) do |t|
+        t.when('a{}') {|v| v.reverse}
+      end
+      expect(result).to eql({'a' => {'b' => 'oof', 'c' => 'rab'}})
+    end
+
+    it 'replaces hash-type hash members' do
+      h = {'a' => {'b' => {'x' => 88}, 'c' => {'x' => 88}}}
+      result = transform_tree2(h) do |t|
+        t.when('a{}') {|v| {'y' => 99}}
+      end
+      expect(result).to eql({'a' => {'b' => {'y' => 99}, 'c' => {'y' => 99}}})
+    end
+
+    it 'replaces array-type hash members' do
+      h = {'a' => {'b' => [1,2,3], 'c' => [4,5,6]}}
+      result = transform_tree2(h) do |t|
+        t.when('a{}') {|v| v.reverse}
+      end
+      expect(result).to eql({'a' => {'b' => [3,2,1], 'c' => [6,5,4]}})
+    end
+
+    it 'replaces primitive-type array members' do
+      h = {'a' => [1, 2]}
+      result = transform_tree2(h) do |t|
+        t.when('a[]') {|v| v + 10}
+      end
+      expect(result).to eql({'a' => [11, 12]})
+    end
+
+    it 'replaces hash-type array members' do
+      h = {'a' => [{'b' => 1}, {'c' => 2}]}
+      result = transform_tree2(h) do |t|
+        t.when('a[]') { {'z' => 99} }
+      end
+      expect(result).to eql({'a' => [{'z' => 99}, {'z' => 99}]})
+    end
+
+    it 'replaces array-type array members' do
+      h = {'a' => [[1,2,3], [4,5,6]]}
+      result = transform_tree2(h) do |t|
+        t.when('a[]') {|v| v.reverse}
+      end
+      expect(result).to eql({'a' => [[3,2,1], [6,5,4]]})
+    end
+
+    context 'mutation' do
+      before(:each) do
+        @old = {'a' => {'x' => 1, 'y' => 2}, 'b' => {'x' => 17, 'y' => 23}}
+        @new = transform_tree2(@old) do |t|
+          t.when('a') {|v|
+            v['z'] = v['x'] + v['y']
+            v
+          }
         end
-        expect(result[:a][:b1][:c]).to eql 'c.1b.a'
-        expect(result[:a][:b2][:c]).to eql 'c.2b.a'
       end
-
-      context 'examples' do
-        it 'replace all ids' do
-          result = transform_tree(hash) do |path, value|
-            case path
-              when '*/_id'
-                value.to_s.reverse
-              else
-                value
-            end
-          end
-          result.to_s
-        end
+      it 'does not mutate the input' do
+        expect(@old).to eql({'a' => {'x' => 1, 'y' => 2}, 'b' => {'x' => 17, 'y' => 23}})
+        expect(@new).to eql({'a' => {'x' => 1, 'y' => 2, 'z' => 3}, 'b' => {'x' => 17, 'y' => 23}})
       end
-
-      it 'when visiting a hash, if the block returns true, hash members are visited' do
-        test_hash_visiting(true, nil)
-      end
-
-      it 'when visiting a hash, if the block returns false, hash members are not visited' do
-        test_hash_visiting(false, nil)
-      end
-
-      it 'when visiting an array, if the block returns true, array elements are visited' do
-        test_array_visiting(true, nil)
-      end
-
-      it 'when visiting an array, if the block returns false, array elements are not visited' do
-        test_array_visiting(false, nil)
-      end
-
-      def test_hash_visiting(should_visit, replacement)
-        test_visiting(should_visit, replacement, 'a', 'a/b1')
-      end
-
-      def test_array_visiting(should_visit, replacement)
-        test_visiting(should_visit, replacement, 'd', 'd/0')
-      end
-
-      def test_visiting(should_visit, replacement, key, subkey)
-        saw_it = false
-        transform_tree(hash) do |path, value|
-          case path
-            when key
-              [replacement, should_visit]
-            when subkey
-              saw_it = true
-            else
-              value
-          end
-        end
-        expect(saw_it).to eql should_visit
+      it 'preserves unmodified substructure' do
+        expect(@old['a'].equal?(@new['a'])).to be_falsey
+        expect(@old['b'].equal?(@new['b'])).to be_truthy
       end
     end
   end
@@ -127,6 +131,7 @@ describe Abstractivator::TreeVisitor do
     ac_in2 = ac_in.deep_dup
     ac_out2 = nil
     time_it(:ad_hoc) do
+      ###############################################
       ac_out2 = ac_in2.deep_dup
       ac_out2['compound_methods'].each do |cm|
         cal = cm['calibration']
@@ -150,6 +155,7 @@ describe Abstractivator::TreeVisitor do
             rs[k] = v.to_s.reverse
           end
         end
+        ###############################################
 
       end
     end
@@ -157,6 +163,7 @@ describe Abstractivator::TreeVisitor do
     ac_in3 = ac_in.deep_dup
     ac_out3 = nil
     time_it(:transform_tree2) do
+      ###############################################
       ac_out3 = transform_tree2(ac_in3) do |t|
         t.when('compound_methods/calibration/normalizers[]') {|v| v.to_s.reverse}
         t.when('compound_methods/calibration/responses[]') {|v| v.to_s.reverse}
@@ -169,6 +176,7 @@ describe Abstractivator::TreeVisitor do
           ret_time
         end
       end
+      ###############################################
     end
 
     # File.write('a.json', JSON.dump(ac_tt))
