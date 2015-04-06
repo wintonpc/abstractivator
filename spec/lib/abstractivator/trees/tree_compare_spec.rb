3ab723113aa30b0719b08ab0d3a58ba8a760011a
Peter Winton <pwinton@indigobio.com>
2015-04-06 14:47:56 -0400

    Removed 'example' method in tree_compare_spec

--- spec/lib/abstractivator/trees/tree_compare_spec.rb
+++ spec/lib/abstractivator/trees/tree_compare_spec.rb
@@ -12,154 +12,175 @@ describe Abstractivator::Trees do
 
     extend Abstractivator::Trees
 
-    def self.example(description, values)
-      it description do
-        tree, mask, expected = values[:tree], values[:mask], values[:result]
-        expect(tree_compare(tree, mask)).to eql expected
-      end
+    def verify(values)
+      tree, mask, expected = values[:tree], values[:mask], values[:result]
+      expect(tree_compare(tree, mask)).to eql expected
     end
 
-    example 'returns an empty list if the tree is comparable to the mask',
-            tree:   {a: 1},
-            mask:   {a: 1},
-            result: []
+    it 'returns an empty list if the tree is comparable to the mask' do
+      verify tree:   {a: 1},
+             mask:   {a: 1},
+             result: []
+    end
 
-    example 'only requires the mask to match a subtree',
-            tree:   {a: 1, b: 1},
-            mask:   {a: 1},
-            result: []
+    it 'only requires the mask to match a subtree' do
+      verify tree:   {a: 1, b: 1},
+             mask:   {a: 1},
+             result: []
+    end
 
-    example 'returns a list of differences',
-            tree:   {a: 1, b: {c: [8, 8]}},
-            mask:   {a: 2, b: {c: [8, 9]}},
-            result: [{path: 'a', tree: 1, mask: 2},
-                     {path: 'b/c/1', tree: 8, mask: 9}]
+    it 'returns a list of differences' do
+      verify tree:   {a: 1, b: {c: [8, 8]}},
+             mask:   {a: 2, b: {c: [8, 9]}},
+             result: [{path: 'a', tree: 1, mask: 2},
+                      {path: 'b/c/1', tree: 8, mask: 9}]
+    end
 
-    example 'returns a list of differences for missing values',
-            tree:   {},
-            mask:   {a: 2, b: nil},
-            result: [{path: 'a', tree: :__missing__, mask: 2}, {path: 'b', tree: :__missing__, mask: nil}]
+    it 'returns a list of differences for missing values' do
+      verify tree:   {},
+             mask:   {a: 2, b: nil},
+             result: [{path: 'a', tree: :__missing__, mask: 2}, {path: 'b', tree: :__missing__, mask: nil}]
+    end
 
-    example 'compares hash values',
-            tree:   {a: 1},
-            mask:   {a: 2},
-            result: [{path: 'a', tree: 1, mask: 2}]
+    it 'compares hash values' do
+      verify tree:   {a: 1},
+             mask:   {a: 2},
+             result: [{path: 'a', tree: 1, mask: 2}]
+    end
 
-    example 'compares array values',
-            tree:   {a: [1, 2]},
-            mask:   {a: [1, 3]},
-            result: [{path: 'a/1', tree: 2, mask: 3}]
+    it 'compares array values' do
+      verify tree:   {a: [1, 2]},
+             mask:   {a: [1, 3]},
+             result: [{path: 'a/1', tree: 2, mask: 3}]
+    end
 
-    example 'compares with predicates',
-            tree:   {a: 1},
-            mask:   {a: proc {|x| x.even?}},
-            result: [{path: 'a', tree: 1, mask: 'proc { |x| x.even? }'}]
+    it 'compares with predicates' do
+      verify tree:   {a: 1},
+             mask:   {a: proc {|x| x.even?}},
+             result: [{path: 'a', tree: 1, mask: 'proc { |x| x.even? }'}]
+    end
 
-    example 'compares with predicates (degrades gracefully when source code is unavailable)',
-            tree:   {a: 1},
-            mask:   {a: :even?.to_proc},
-            result: [{path: 'a', tree: 1, mask: :__predicate__}]
+    it 'compares with predicates (degrades gracefully when source code is unavailable)' do
+      verify tree:   {a: 1},
+             mask:   {a: :even?.to_proc},
+             result: [{path: 'a', tree: 1, mask: :__predicate__}]
+    end
 
     it 'compares with predicates (lets non-sourcify errors through)' do
       expect{tree_compare({a: 1}, {a: proc { raise 'oops' }})}.to raise_error
     end
 
-    example 'can ensure values are absent with :-',
-            tree:   {a: 1},
-            mask:   {a: :-},
-            result: [{path: 'a', tree: 1, mask: :__absent__}]
+    it 'can ensure values are absent with :-' do
+      verify tree:   {a: 1},
+             mask:   {a: :-},
+             result: [{path: 'a', tree: 1, mask: :__absent__}]
+    end
 
-    example 'can check for any value being present with :+',
-            tree:   {a: 1, b: [1, 2, 3]},
-            mask:   {a: :+, b: [1, :+, 3]},
-            result: []
+    it 'can check for any value being present with :+' do
+      verify tree:   {a: 1, b: [1, 2, 3]},
+             mask:   {a: :+, b: [1, :+, 3]},
+             result: []
+    end
 
     context 'when comparing arrays' do
-      example 'reports the tree being shorter',
-              tree:   {a: [1]},
-              mask:   {a: [1, 2]},
-              result: [{path: 'a/1', tree: :__missing__, mask: [2]}]
+      it 'reports the tree being shorter' do
+        verify tree:   {a: [1]},
+               mask:   {a: [1, 2]},
+               result: [{path: 'a/1', tree: :__missing__, mask: [2]}]
+      end
 
-      example 'reports the mask being shorter',
-              tree:   {a: [1, 2]},
-              mask:   {a: [1]},
-              result: [{path: 'a/1', tree: [2], mask: :__absent__}]
+      it 'reports the mask being shorter' do
+        verify tree:   {a: [1, 2]},
+               mask:   {a: [1]},
+               result: [{path: 'a/1', tree: [2], mask: :__absent__}]
+      end
 
-      example 'can allow arbitrary tails with :*',
-              tree:   {a: [1, 2, 3], b: [1], c: [2]},
-              mask:   {a: [1, :*], b: [1, :*], c: [1, :*]},
-              result: [{path: 'c/0', tree: 2, mask: 1}]
+      it 'can allow arbitrary tails with :*' do
+        verify tree:   {a: [1, 2, 3], b: [1], c: [2]},
+               mask:   {a: [1, :*], b: [1, :*], c: [1, :*]},
+               result: [{path: 'c/0', tree: 2, mask: 1}]
+      end
     end
 
     context 'when comparing sets' do
 
-      def self.get_name
+      def get_name
         ->(x){ x[:name] }
       end
 
-      example 'allows out-of-order arrays',
-              tree:   {set: [{id: 2, name: 'b'}, {id: 1, name: 'a'}]},
-              mask:   {set: set_mask([{id: 1, name: 'a'}, {id: 2, name: 'b'}], get_name)},
-              result: []
+      it 'allows out-of-order arrays' do
+        verify tree:   {set: [{id: 2, name: 'b'}, {id: 1, name: 'a'}]},
+               mask:   {set: set_mask([{id: 1, name: 'a'}, {id: 2, name: 'b'}], get_name)},
+               result: []
+      end
 
-      example 'reports missing set attribute in the tree',
-              tree:   {},
-              mask:   {set: set_mask([{id: 1, name: 'a'}], get_name)},
-              result: [{path: 'set', tree: :__missing__, mask: [{id: 1, name: 'a'}]}]
+      it 'reports missing set attribute in the tree' do
+        verify tree:   {},
+               mask:   {set: set_mask([{id: 1, name: 'a'}], get_name)},
+               result: [{path: 'set', tree: :__missing__, mask: [{id: 1, name: 'a'}]}]
+      end
 
-      example 'reports missing items in the tree',
-              tree:   {set: []},
-              mask:   {set: set_mask([{id: 1, name: 'a'}], get_name)},
-              result: [{path: 'set/a', tree: :__missing__, mask: {id: 1, name: 'a'}}]
+      it 'reports missing items in the tree' do
+        verify tree:   {set: []},
+               mask:   {set: set_mask([{id: 1, name: 'a'}], get_name)},
+               result: [{path: 'set/a', tree: :__missing__, mask: {id: 1, name: 'a'}}]
+      end
 
-      example 'reports extra items in the tree',
-              tree:   {set: [{id: 1, name: 'a'}]},
-              mask:   {set: set_mask([], get_name)},
-              result: [{path: 'set/a', tree: {id: 1, name: 'a'}, mask: :__absent__}]
+      it 'reports extra items in the tree' do
+        verify tree:   {set: [{id: 1, name: 'a'}]},
+               mask:   {set: set_mask([], get_name)},
+               result: [{path: 'set/a', tree: {id: 1, name: 'a'}, mask: :__absent__}]
+      end
 
-      example 'reports duplicate keys in the tree',
-              tree:   {set: [{id: 1, name: 'a'}, {id: 2, name: 'a'}]},
-              mask:   {set: set_mask([:*], get_name)},
-              result: [{path: 'set', tree: [:__duplicate_keys__, ['a']], mask: nil}]
+      it 'reports duplicate keys in the tree' do
+        verify tree:   {set: [{id: 1, name: 'a'}, {id: 2, name: 'a'}]},
+               mask:   {set: set_mask([:*], get_name)},
+               result: [{path: 'set', tree: [:__duplicate_keys__, ['a']], mask: nil}]
+      end
 
-      example 'reports duplicate keys in the mask',
-              tree:   {set: [{id: 1, name: 'a'}]},
-              mask:   {set: set_mask([{id: 1, name: 'a'}, {id: 2, name: 'a'}], get_name)},
-              result: [{path: 'set', tree: nil, mask: [:__duplicate_keys__, ['a']]}]
+      it 'reports duplicate keys in the mask' do
+        verify tree:   {set: [{id: 1, name: 'a'}]},
+               mask:   {set: set_mask([{id: 1, name: 'a'}, {id: 2, name: 'a'}], get_name)},
+               result: [{path: 'set', tree: nil, mask: [:__duplicate_keys__, ['a']]}]
+      end
 
-      example 'can test for only a subset',
-              tree:   {set: [{id: 1, name: 'a'}, {id: 2, name: 'b'}]},
-              mask:   {set: set_mask([{id: 2, name: 'b'}, :*], get_name)},
-              result: []
+      it 'can test for only a subset' do
+        verify tree:   {set: [{id: 1, name: 'a'}, {id: 2, name: 'b'}]},
+               mask:   {set: set_mask([{id: 2, name: 'b'}, :*], get_name)},
+               result: []
+      end
     end
 
-
-
     context 'reports mismatched types' do
-      example 'hash for primitive',
-              tree:   {a: {b: 1}},
-              mask:   {a: 1},
-              result: [{path: 'a', tree: {b: 1}, mask: 1}]
+      it 'hash for primitive' do
+        verify tree:   {a: {b: 1}},
+               mask:   {a: 1},
+               result: [{path: 'a', tree: {b: 1}, mask: 1}]
+      end
 
-      example 'primitive for hash',
-              tree:   {a: 1},
-              mask:   {a: {b: 1}},
-              result: [{path: 'a', tree: 1, mask: {b: 1}}]
+      it 'primitive for hash' do
+        verify tree:   {a: 1},
+               mask:   {a: {b: 1}},
+               result: [{path: 'a', tree: 1, mask: {b: 1}}]
+      end
 
-      example 'array for primitive',
-              tree:   {a: [1, 2]},
-              mask:   {a: 1},
-              result: [{path: 'a', tree: [1, 2], mask: 1}]
+      it 'array for primitive' do
+        verify tree:   {a: [1, 2]},
+               mask:   {a: 1},
+               result: [{path: 'a', tree: [1, 2], mask: 1}]
+      end
 
-      example 'primitive for array',
-              tree:   {a: 1},
-              mask:   {a: [1, 2]},
-              result: [{path: 'a', tree: 1, mask: [1, 2]}]
+      it 'primitive for array' do
+        verify tree:   {a: 1},
+               mask:   {a: [1, 2]},
+               result: [{path: 'a', tree: 1, mask: [1, 2]}]
+      end
 
-      example 'primitive for set',
-              tree:   {set: 1},
-              mask:   {set: set_mask([{x: 1}], ->(item) { item[:x] })},
-              result: [{path: 'set', tree: 1, mask: [{x: 1}]}]
+      it 'primitive for set' do
+        verify tree:   {set: 1},
+               mask:   {set: set_mask([{x: 1}], ->(item) { item[:x] })},
+               result: [{path: 'set', tree: 1, mask: [{x: 1}]}]
+      end
     end
   end
 end
